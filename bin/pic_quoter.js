7dd67447c2d86a922a0c75480ab496c1e4d5d7c9
Fredrik Christenson <fredrik.christenson@ticnet.se>
2016-11-10 16:49:11 +0100

    added pic

--- bin/pic_quoter.js
+++ bin/pic_quoter.js
@@ -1,32 +1,27 @@
-var Log         = require('../lib/utils/logging_utils');
+var Log    = require('../lib/utils/logging_utils');
+var client = require('../lib/client');
 
 var casper = require('casper').create({
   pageSettings: {
-    loadImages:  false,
+    loadImages:  true,
     loadPlugins: false
   }
 });
 
-var IMG = '.bqPhotoDefault';
+var IMG_FILE_NAME = 'quote_img.png';
+var IMG_URL       = '';
 
 casper.start('http://www.brainyquote.com/quotes_of_the_day.html');
 
 casper.then(Log.logStartTime);
 
-casper.then(function() {
-  this.echo('--- Quote Image ---');
-});
-
-casper.then(Log.logEndLine);
-
-require('../lib/then_login');
+casper.then(Log.makeEcho('--- Quote Image ---'));
 
 casper.then(function() {
-  this.echo('--- Post quote ---');
+  IMG_URL = this.evaluate(client.findQuoteImage);
+  if(IMG_URL) {
+    this.download(IMG_URL, IMG_FILE_NAME);
+  }
 });
-
-casper.then(Log.logEndLine);
-
-casper.then(Log.logEndTime);
 
 casper.run();
